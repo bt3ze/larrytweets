d38f646effb287b1a2e3deb4de0e627a62f40534
Ubuntu <ubuntu@ip-172-31-37-236.ec2.internal>
2014-04-24 19:16:40 +0000

    cleaned up

--- larry.py
+++ larry.py
@@ -11,18 +11,13 @@ from pymongo import ASCENDING, DESCENDING
 db = client.cs4501
 tweets = db.tweets
 
-reldist = []
-distset = Set()
+ary = []
 
 for tweet in tweets.find( { "retweeted_status.id" : {"$ne":"null"} , "user.screen_name": "LarrySabato" }, { "text": 1 , "retweet_count":1, "created_at":1, "user.screen_name":1 }).sort("retweet_count",DESCENDING):
-#    print tweet                                                                  
-    if not tweet["text"] in distset:
-        if u'RT' in tweet["text"]:
-            reldist.append(tweet)
-#            distset.add(tweet["text"])
+    if u'RT' in tweet["text"]:
+        ary.append(tweet)
 
-for t in reldist:
+for t in ary:
     print t
 
-#print "done"
 
--- larryrt.py
+++ larryrt.py
@@ -11,18 +11,6 @@ from pymongo import ASCENDING, DESCENDING
 db = client.cs4501
 tweets = db.tweets
 
-reldist = []
-distset = Set()
 
 for tweet in tweets.find( { "retweeted_status.id" : {"$ne":"null"}, "retweet_count": {"$lt":433 }, "retweeted_status.user.screen_name": "LarrySabato" }, { "text": 1 , "retweet_count":1, "created_at":1, "user.screen_name":1 }).sort("retweet_count",DESCENDING):
     print tweet                                                                                            
-#    if not tweet["text"] in distset:
-#        reldist.append(tweet)
-#        distset.add(tweet["text"])
-
-#for t in reldist:
-#    print t
-
-#print "done"
-
-
